4db93fa893d7c7b0110e6d3fce304c969004bc83
Rodney <rodney.zhang@pointclickcare.com>
2024-08-03 22:23:38 -0400

    init commit

---
+++ PracticeProject/PracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by Rodney Zhang on 2024-08-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
